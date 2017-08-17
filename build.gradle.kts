01b4b52fdf41970b4794bf42db824a507ab63d32
crummy <crummynz@gmail.com>
2017-08-17 13:04:51 +0200

    Finally have shadowJar building, but it's missing main manifest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,43 @@
-import org.gradle.kotlin.dsl.kotlin
+
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.kotlin.dsl.invoke
 import org.gradle.kotlin.dsl.repositories
 import java.net.URI
+
+buildscript {
+    repositories {
+        maven {
+            setUrl("https://plugins.gradle.org/m2/")
+        }
+    }
+    dependencies {
+        classpath("com.github.jengelman.gradle.plugins:shadow:2.0.0")
+    }
+}
 
 subprojects {
     buildscript {
         repositories {
             mavenCentral()
+            jcenter()
         }
     }
 
     group = "com.malcolmcrum.photolocation"
     version = "1.0"
 
-    plugins {
-        kotlin("jvm")
-    }
 
     repositories {
         maven { url = URI("http://download.osgeo.org/webdav/geotools/") }
-
         jcenter()
     }
+
+    tasks {
+        "shadowJar"(ShadowJar::class) {
+            baseName = "${project.name}-all"
+            classifier = null
+            version = null
+        }
+    }
 }
+
